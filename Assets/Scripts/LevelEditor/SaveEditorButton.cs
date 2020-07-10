1c3016dfb671450a0f5f4967af53974557e29446
Jake Yeatman <jake.yeatman@rds.global>
2020-07-11 00:59:34 +0100

    Start of saving

---
+++ Assets/Scripts/LevelEditor/SaveEditorButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Dogelix.LevelEditor
+{
+    public class SaveEditorButton : MonoBehaviour
+    {
+        public void SaveFormat()
+        {
+            var name = transform.parent.GetComponentInChildren<TMP_InputField>().text;
+            Debug.Log(name);
+        }
+    }
+}
