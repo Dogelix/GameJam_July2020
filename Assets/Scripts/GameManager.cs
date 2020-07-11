45516b104740af9fafbaf4914dbd54e864c7ed37
Jake Yeatman <jake.yeatman@rds.global>
2020-07-11 20:11:48 +0100

    Core gameplay done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Dogelix;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,27 +11,42 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        string[] stringSeparators = new string[] { "\r\n" };
-        string[] lines = _levelData.text.Split(stringSeparators, System.StringSplitOptions.None);
-
-        foreach ( var line in lines )
+        if(_levelData != null)
         {
-            if ( line.Contains(":") )
+            string[] stringSeparators = new string[] { "\r\n" };
+            string[] lines = _levelData.text.Split(stringSeparators, System.StringSplitOptions.None);
+
+            foreach ( var line in lines )
             {
-                var split = line.Split(new string[] { ":" }, StringSplitOptions.None);
-                EBlockType blockType = (EBlockType)Enum.Parse(typeof(EBlockType), split[1]);
+                if ( line.Contains(":") )
+                {
+                    var split = line.Split(new string[] { ":" }, StringSplitOptions.None);
+                    EBlockType blockType = (EBlockType)Enum.Parse(typeof(EBlockType), split[1]);
 
-                var splitPos = split[2].Split(new string[] { "," }, StringSplitOptions.None);
-                Vector3 location = new Vector3(int.Parse(splitPos[0]), int.Parse(splitPos[1]), int.Parse(splitPos[2]));
+                    var splitPos = split[2].Split(new string[] { "," }, StringSplitOptions.None);
+                    Vector3 location = new Vector3(float.Parse(splitPos[0]), float.Parse(splitPos[1]), float.Parse(splitPos[2]));
 
-                var splitRot = split[3].Split(new string[] { "," }, StringSplitOptions.None);
-                Vector3 rot = new Vector3(int.Parse(splitRot[0]), int.Parse(splitRot[1]), int.Parse(splitRot[2]));
+                    var splitRot = split[3].Split(new string[] { "," }, StringSplitOptions.None);
+                    Vector3 rot = new Vector3(float.Parse(splitRot[0]), float.Parse(splitRot[1]), float.Parse(splitRot[2]));
 
-                Block.Create(blockType, location, rot);
+                    if(blockType == EBlockType.Start || blockType == EBlockType.Goal )
+                    {
+                        Block.Create(blockType, new Vector3(location.x, 1, location.z), rot);
+                    }
+                    else
+                    {
+                        var t = Block.Create(blockType, location, rot);
+                        t.GetComponent<Rigidbody>().useGravity = true;
+                    }
+
+                }
             }
-        }
 
-        Debug.Log(_levelData.text);
+            var start  = GameObject.FindObjectsOfType<Block>().First(e => e._type.EBlockType == EBlockType.Start);
+
+            Instantiate(GameAssets.i.Player, start.transform.position, Quaternion.identity);
+        }
+        
     }
 
     private void LateUpdate()
--- Assets/Scripts/LevelEditor/SaveEditorButton.cs
+++ Assets/Scripts/LevelEditor/SaveEditorButton.cs
@@ -22,42 +22,18 @@ namespace Dogelix.LevelEditor
             //{
             //    Debug.Log(item.name);
             //}
-            EditorGrid grid = FindObjectOfType<EditorGrid>();
+            //EditorGrid grid = FindObjectOfType<EditorGrid>();
 
             using ( StreamWriter sw = new StreamWriter(path) )
             {
-                // some math to find the most left and bottom offset
-                float offsetLeft = (-grid._size.Width/2f)*grid._size.DistanceX + grid._size.DistanceX/2f;
-                float offsetBottom = (-grid._size.Length/2f)*grid._size.DistanceY + grid._size.DistanceY/2f;
-
-                // set it as first spawn position (z=1 because you had it in your script)
-                Vector3 nextPosition = new Vector3(offsetLeft, 0f, offsetBottom);
-
-                string dataToWrite = "";
-
-                for ( int y = 0; y < grid._size.Length; y++ )
+                foreach ( var block in blockObjects )
                 {
-                    for ( int x = 0; x < grid._size.Width; x++ )
-                    {
-                        dataToWrite = x.ToString() + y.ToString();
+                    string dataToWrite = "block";
 
-                        if(blockObjects.FirstOrDefault(e => e.transform.position == ( nextPosition + Vector3.up )) != null )
-                        {
-                            var blockData = blockObjects.First(e => e.transform.position == ( nextPosition + Vector3.up ));
-                            var b = blockData.GetComponent<Block>();
-                            dataToWrite += ":" + b._type.EBlockType.ToString() + String.Format(":{0},{1},{2}", blockData.transform.position.x, blockData.transform.position.y, blockData.transform.position.z) +
-                                String.Format(":{0},{1},{2}", blockData.transform.rotation.eulerAngles.x, blockData.transform.rotation.eulerAngles.y, blockData.transform.rotation.eulerAngles.z);
-                        }
-
-                        sw.WriteLine(dataToWrite);
-
-                        // add x distance
-                        nextPosition.x += grid._size.DistanceX;
-                    }
-                    // reset x position and add y distance
-                    nextPosition.x = offsetLeft;
-                    nextPosition.z += grid._size.DistanceY;
-                    //Debug.Log(nextPosition);
+                    var b = block.GetComponent<Block>();
+                    dataToWrite += ":" + b._type.EBlockType.ToString() + String.Format(":{0},{1},{2}", block.transform.position.x, block.transform.position.y, block.transform.position.z) +
+                        String.Format(":{0},{1},{2}", block.transform.rotation.eulerAngles.x, block.transform.rotation.eulerAngles.y, block.transform.rotation.eulerAngles.z);
+                    sw.WriteLine(dataToWrite);
                 }
             }
 
